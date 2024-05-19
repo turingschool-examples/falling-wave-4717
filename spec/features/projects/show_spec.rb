be96b0e6b61e9900706f01d5d834e0f5f7aff84d
Zach Bergman <bzach634@gmail.com>
2024-05-19 11:00:57 -0600

    test: Add tests for number of contestants on view

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe "Project Show Page" do
   before(:each) do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
   end
 
   describe "As a visitor" do
@@ -19,6 +25,12 @@ RSpec.describe "Project Show Page" do
         visit("/projects/#{@news_chic.id}")
 
         expect(page).to have_content(@recycled_material_challenge.theme)
+      end
+
+      it "shows a count of the number of contestants on this project" do
+        visit("/projects/#{@news_chic.id}")
+
+        expect(page).to have_content("Number of Contestants: 2")
       end
     end
   end
