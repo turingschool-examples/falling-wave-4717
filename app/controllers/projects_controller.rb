9b200160e03ea32562aa1f3180b423090ec544c4
Zach Bergman <bzach634@gmail.com>
2024-05-19 11:27:47 -0600

    feat: Add average_years_exp instance variable to show action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,5 +4,6 @@ class ProjectsController < ApplicationController
     @project_material = @project.material
     @project_challenge_theme = @project.challenge.theme
     @contestant_count = @project.contestant_count
+    @average_years_exp = @project.average_years_exp
   end
 end
