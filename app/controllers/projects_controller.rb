2e6f782ae13b995f57598c015111ee7ce9bd1237
Zach Bergman <bzach634@gmail.com>
2024-05-19 11:02:32 -0600

    feat: Add contestant_count to show action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,5 +3,6 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     @project_material = @project.material
     @project_challenge_theme = @project.challenge.theme
+    @contestant_count = @project.contestant_count
   end
 end
