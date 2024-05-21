1d0ae9f2b4f4154085c27f0849bea132d890c886
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-20 21:55:57 -0600

    US3

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -5,12 +5,17 @@ RSpec.describe "Project Show", type: :feature do
     scenario "when I visit the show page I should see" do
       recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
       news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
 
       visit "/projects/#{news_chic.id}"
       save_and_open_page
       expect(page).to have_content("Project Name: News Chic")
       expect(page).to have_content("Material: Newspaper")
       expect(page).to have_content("Challenge Theme: Recycled Material")
+      expect(page).to have_content("Number of Contestants: 2")
     end
 
   end
