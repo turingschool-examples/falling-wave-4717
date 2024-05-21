8fabf381ef24650f879040408946e9bdda9aa454
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-21 16:51:17 -0500

    Added test for contest instance method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "instance methods" do
+    it "makes a string of project names" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+
+      expect(jay.project_names).to eq("News Chic")
+    end
+  end
 end
