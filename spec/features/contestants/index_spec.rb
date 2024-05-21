ff6410d2188b6a284a3297f444e9307746546f79
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-21 17:11:50 -0500

    Verified view on local host

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -11,13 +11,13 @@ describe "Contestants Index Page" do
 
         upholstery_tux = furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
 
-        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
 
 
-        ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-        ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-        ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+        ContestantProject.create!(contestant_id: jay.id, project_id: news_chic.id)
+        ContestantProject.create!(contestant_id: gretchen.id, project_id: news_chic.id)
+        ContestantProject.create!(contestant_id: gretchen.id, project_id: upholstery_tux.id)
 
         visit "/contestants"
         within("#contestant-#{jay.id}") do
