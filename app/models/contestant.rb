63963a7925c79a91e50e5883178d79d36af95875
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-21 17:22:17 -0500

    Changed model .map to pluck

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -3,7 +3,8 @@ class Contestant < ApplicationRecord
     has_many :projects, through: :contestant_projects
 
     def project_names
-        projects.map(&:name).join(", ")
+        projects.pluck(:name).join(", ")
+        # is it safe to use join here or should i use join in the view?
     end
 
     def self.average_years_of_experience
