13481f6be8a0aa950941b20d47b4cd1015a63b6e
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-23 13:55:41 -0600

    Remove whitespace

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Challenge, type: :model do
-
   describe "relationships" do
     it {should have_many :projects}
   end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Project, type: :model do
-
   describe "relationships" do
     it {should belong_to :challenge}
     it {should have_many :contestant_projects}
