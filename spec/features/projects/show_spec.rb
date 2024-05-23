bdb9145a4a112d19d79e652b36bdf11a8de1794e
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-23 14:37:50 -0600

    Begin testing user story one

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe "project show page", type: :feature do
+
+  before(:each) do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    # @ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    # @ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    # @ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    # @ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+  end
+
+  # As a visitor,
+  # When I visit a project's show page ("/projects/:id"),
+  # I see that project's name and material
+  # And I also see the theme of the challenge that this project belongs to.
+  describe "as a visitor" do
+    describe "when I visit a project's show page" do
+      it "I see that project's name and material, and I also see the theme of the challenge that this project belongs to" do
+        visit "/projects/:id"
+
+        require 'pry'; binding.pry
+        # expect(page).to have_content(project.name)
+        # expect(page).to have_content(project.material)
+      end
+    end
+  end
+end
