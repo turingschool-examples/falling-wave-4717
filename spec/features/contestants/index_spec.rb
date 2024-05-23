cf39c5c855553ebc9b793b6d53bcf9c95fd0e8ff
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-23 15:49:29 -0600

    Fix ContestantProject.create by removing @ in front

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -13,10 +13,10 @@ RSpec.describe "contestant index page", type: :feature do
     @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
-    # @ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
-    # @ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
-    # @ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
-    # @ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   # As a visitor,
@@ -29,7 +29,7 @@ RSpec.describe "contestant index page", type: :feature do
         visit "/contestants"
 
         expect(page).to have_content("Name: #{@contestant.name}")
-        expect(page).to have_content("Name: #{@contestant.project}")
+        expect(page).to have_content("Name: #{@project.name}")
       end
     end
   end
